9af9312c8263b5d28bb4dc494f4aeefad6ec8c00
Victoria Blatkowska <victoriablatkowska@gmail.com>
2020-02-11 11:12:25 +0100

    jobs model added

---
+++ models/jobs.js
@@ -0,0 +1,3 @@
+const mongoose = require("mongoose");
+
+const Jobs = mongoose.Schema({});
--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const User = mongoose.Schema({
-	email: { type: String, require: true, unique: true },
+  email: { type: String, require: true, unique: true }
 });
