834a19e4f026417a069ec050b35ad6c0fec7cf64
Jonahtan Saudhof <jonathan@jonathansaudhof.de>
2020-02-14 07:44:46 +0100

    final changes

--- app.js
+++ app.js
@@ -1,40 +1,41 @@
-require("dotenv").config();
+require('dotenv').config();
 
 // general imports
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const express = require("express");
-const favicon = require("serve-favicon");
-const exphbs = require("express-handlebars");
-const mongoose = require("mongoose");
-const logger = require("morgan");
-const path = require("path");
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const favicon = require('serve-favicon');
+const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
+const moment = require('moment');
 
 //authentication imports
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const flash = require("connect-flash");
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
 
 mongoose
-  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
+	.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+	.then((x) => {
+		console.log(
+			`Connected to Mongo! Database name: "${x.connections[0].name}"`,
+		);
+	})
+	.catch((err) => {
+		console.error('Error connecting to mongo', err);
+	});
 
-const app_name = require("./package.json").name;
-const debug = require("debug")(
-  `${app_name}:${path.basename(__filename).split(".")[0]}`
+const app_name = require('./package.json').name;
+const debug = require('debug')(
+	`${app_name}:${path.basename(__filename).split('.')[0]}`,
 );
 
 const app = express();
 
 // Middleware Setup
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -42,61 +43,74 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(
-  require("node-sass-middleware")({
-    src: path.join(__dirname, "public"),
-    dest: path.join(__dirname, "public"),
-    sourceMap: true
-  })
+	require('node-sass-middleware')({
+		src: path.join(__dirname, 'public'),
+		dest: path.join(__dirname, 'public'),
+		sourceMap: true,
+	}),
 );
 
 const hbs = exphbs.create({
-  extname: "hbs",
-  defaultLayout: "main",
-  /* layoutsDir: path.resolve(__dirname, 'views/layouts' */
-  helpers: {
-    equal: function(attribute1, attribute2, options) {
-      console.log(typeof attribute1, typeof attribute2);
-      if (attribute1 === attribute2) {
-        console.log("---> is equal");
-        return options.fn(this);
-      }
-    }
-  }
+	extname: 'hbs',
+	defaultLayout: 'main',
+	/* layoutsDir: path.resolve(__dirname, 'views/layouts' */
+	helpers: {
+		equal: function(attribute1, attribute2, options) {
+			console.log(typeof attribute1, typeof attribute2);
+			if (attribute1 === attribute2) {
+				console.log('---> is equal');
+				return options.fn(this);
+			}
+		},
+		formatDate: function(datetime, format) {
+			const DateFormats = {
+				short: 'DD MMMM - YYYY',
+				long: 'dddd DD.MM.YYYY HH:mm',
+			};
+			if (moment) {
+				// can use other formats like 'lll' too
+				format = DateFormats[format] || format;
+				return moment(datetime).format(format);
+			} else {
+				return datetime;
+			}
+		},
+	},
 });
 
-app.engine("hbs", hbs.engine);
-app.set("view engine", "hbs");
-app.set("views", path.resolve(__dirname, "views"));
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.resolve(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // default value for title local
-app.locals.title = "Ironhack-Connect";
+app.locals.title = 'Ironhack-Connect';
 
 app.use(
-  session({
-    secret: "irongenerator",
-    resave: true,
-    saveUninitialized: true,
-    store: new MongoStore({ mongooseConnection: mongoose.connection })
-  })
+	session({
+		secret: 'irongenerator',
+		resave: true,
+		saveUninitialized: true,
+		store: new MongoStore({ mongooseConnection: mongoose.connection }),
+	}),
 );
 app.use(flash());
-require("./passport")(app);
+require('./passport')(app);
 
-const index = require("./routes/index");
-const authRoute = require("./routes/auth/index");
-app.use("/", index);
-app.use("/", authRoute); //
+const index = require('./routes/index');
+const authRoute = require('./routes/auth/index');
+app.use('/', index);
+app.use('/', authRoute); //
 app.use((req, res, next) => {
-  res.locals.user = req.user;
-  next();
+	res.locals.user = req.user;
+	next();
 });
-const userRoute = require("./routes/user");
-app.use("/user", userRoute);
+const userRoute = require('./routes/user');
+app.use('/user', userRoute);
 
-const jobsRoute = require("./routes/jobs");
-app.use("/jobs", jobsRoute);
+const jobsRoute = require('./routes/jobs');
+app.use('/jobs', jobsRoute);
 //Users route
 module.exports = app;
--- routes/jobs/index.js
+++ routes/jobs/index.js
@@ -4,171 +4,184 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../../models/jobs');
 
+const checkForLogin = (req, res, next) => {
+	console.log('-----> check for login');
+	if (!req.user) {
+		console.log('-----> not logged in');
+		res.redirect('/');
+		return;
+	}
+	console.log('-----> logged in');
+	next();
+};
+
+router.use(checkForLogin);
+
 /*Add new Jobs*/
 
 router.get('/', (req, res) => {
-  console.log('add new route');
-  res.render('job/dashboard');
+	console.log('add new route');
+	res.render('job/dashboard');
 });
 
 router.get('/new', (req, res) => {
-  console.log('add new route');
-  res.render('job/addNewJob');
+	console.log('add new route');
+	res.render('job/addNewJob');
 });
 
 router.post('/', (req, res, next) => {
-  console.log({ ...req.body });
-  let image = '';
+	console.log({ ...req.body });
+	let image = '';
 
-  switch (req.body.category) {
-    case 'Data':
-      image = '/images/da.svg';
-      break;
-    case 'UX/UI':
-      image = '/images/ux.svg';
-      break;
-    case 'WebDev':
-      image = '/images/wd.svg';
-      break;
-    case '':
-      image = '/images/loudspeaker.png';
-      break;
-  }
+	switch (req.body.category) {
+		case 'Data':
+			image = '/images/da.svg';
+			break;
+		case 'UX/UI':
+			image = '/images/ux.svg';
+			break;
+		case 'WebDev':
+			image = '/images/wd.svg';
+			break;
+		case '':
+			image = '/images/loudspeaker.png';
+			break;
+	}
 
-  Job.create({ ...req.body, image: image, author: req.user._id })
-    .then((createdJob) => {
-      // res.send(createdJob);
-      res.redirect(`jobs/${createdJob._id}`);
-    })
-    .catch((err) => {
-      next(err);
-    });
+	Job.create({ ...req.body, image: image, author: req.user._id })
+		.then((createdJob) => {
+			// res.send(createdJob);
+			res.redirect(`jobs/${createdJob._id}`);
+		})
+		.catch((err) => {
+			next(err);
+		});
 
-  // Job.create({
-  //   title: res.body.title,
-  //   company: res.body.company,
-  //   companyUrl: res.body.companyUrl,
-  //   description: res.body.description,
-  //   contractType: res.body.contractType,
-  //   industry: res.body.industry,
-  //   category: res.body.category,
-  //   location: res.body.location,
-  //   author: res.body.author,
-  //   startsAt: res.body.startsAt,
-  // })
-  //   .then((createdJob) => {
-  //     res.redirect('/jobs/${createdJob._id}');
-  //   })
-  //   .catch((err) => {
-  //     next(err);
-  //   });
+	// Job.create({
+	//   title: res.body.title,
+	//   company: res.body.company,
+	//   companyUrl: res.body.companyUrl,
+	//   description: res.body.description,
+	//   contractType: res.body.contractType,
+	//   industry: res.body.industry,
+	//   category: res.body.category,
+	//   location: res.body.location,
+	//   author: res.body.author,
+	//   startsAt: res.body.startsAt,
+	// })
+	//   .then((createdJob) => {
+	//     res.redirect('/jobs/${createdJob._id}');
+	//   })
+	//   .catch((err) => {
+	//     next(err);
+	//   });
 });
 
 /*Job details*/
 router.get('/:id', (req, res) => {
-  console.log('Hello');
+	console.log('Hello');
 
-  const jobId = req.params.id;
-  const userId = req.user._id;
-  // userId --> req.user._id
-  // Job.author._id -->
-  Job.findById(jobId)
-    .populate('author')
-    .then((JobInfo) => {
-      console.log(JobInfo);
+	const jobId = req.params.id;
+	const userId = req.user._id;
+	// userId --> req.user._id
+	// Job.author._id -->
+	Job.findById(jobId)
+		.populate('author')
+		.then((JobInfo) => {
+			console.log(JobInfo);
 
-      if (jobId === userId || req.user.role === 'admin') {
-        const j = JobInfo.toString();
+			if (jobId === userId || req.user.role === 'admin') {
+				const j = JobInfo.toString();
 
-        res.render('job/jobPage.hbs', { jobInfo: JobInfo, allowed: true });
-        return;
-      }
+				res.render('job/jobPage.hbs', { jobInfo: JobInfo, allowed: true });
+				return;
+			}
 
-      res.render('job/jobPage.hbs', { jobInfo: JobInfo });
-    })
-    .catch((err) => {
-      next(err);
-    });
+			res.render('job/jobPage.hbs', { jobInfo: JobInfo });
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 /*Delete Jobs*/
 router.get('/:id/delete', (req, res) => {
-  Job.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.render('job/deleted.hbs');
-      // res.redirect('/jobs');
-    })
-    .catch((err) => {
-      next(err);
-    });
+	Job.deleteOne({ _id: req.params.id })
+		.then(() => {
+			res.render('job/deleted.hbs');
+			// res.redirect('/jobs');
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 /*Edit Jobs*/
 
 router.get('/:id/edit', (req, res, next) => {
-  Job.findById(req.params.id)
-    .then((jobDocuments) => {
-      res.render('job/jobsEdit.hbs', jobDocuments);
-    })
-    .catch((err) => {
-      next(err);
-    });
+	Job.findById(req.params.id)
+		.then((jobDocuments) => {
+			res.render('job/jobsEdit.hbs', jobDocuments);
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 router.post('/:id', (req, res, next) => {
-  const {
-    title,
-    companyUrl,
-    description,
-    contractType,
-    category,
-    location,
-  } = req.body;
-  Job.updateOne(
-    { _id: req.params.id },
-    {
-      title,
-      companyUrl,
-      description,
-      contractType,
-      category,
-      location,
-    },
-  )
-    .then(() => {
-      res.redirect(`/jobs/${req.params.id}`);
-    })
-    .catch((err) => {
-      next(err);
-    });
+	const {
+		title,
+		companyUrl,
+		description,
+		contractType,
+		category,
+		location,
+	} = req.body;
+	Job.updateOne(
+		{ _id: req.params.id },
+		{
+			title,
+			companyUrl,
+			description,
+			contractType,
+			category,
+			location,
+		},
+	)
+		.then(() => {
+			res.redirect(`/jobs/${req.params.id}`);
+		})
+		.catch((err) => {
+			next(err);
+		});
 });
 
 /*Job search*/
 
 router.get('/jobs-search/queries', (request, response) => {
-  const userInput = new RegExp(request.query.job, 'i');
-  Job.find({
-    $or: [
-      { title: userInput },
-      { description: userInput },
-      { companyUrl: userInput },
-      { industry: userInput },
-      { category: userInput },
-    ],
-  })
-    .sort({ created_at: -1 })
-    .then((data) => {
-      //console.log("recieved data:", data);
-      console.log(data);
-      if (data.length === 0) {
-        response.render('job/dashboard.hbs', {
-          message: 'No results for your search',
-        });
-        return;
-      }
+	const userInput = new RegExp(request.query.job, 'i');
+	Job.find({
+		$or: [
+			{ title: userInput },
+			{ description: userInput },
+			{ companyUrl: userInput },
+			{ industry: userInput },
+			{ category: userInput },
+		],
+	})
+		.sort({ created_at: -1 })
+		.then((data) => {
+			//console.log("recieved data:", data);
+			console.log(data);
+			if (data.length === 0) {
+				response.render('job/dashboard.hbs', {
+					message: 'No results for your search',
+				});
+				return;
+			}
 
-      response.render('job/dashboard.hbs', { jobSearchList: data });
-    })
-    .catch((err) => console.log('an errror occured', err));
+			response.render('job/dashboard.hbs', { jobSearchList: data });
+		})
+		.catch((err) => console.log('an errror occured', err));
 });
 
 module.exports = router;
